a0133dda7589adcda5a9318ae910fc7c57375f94
ye <2012837965@qq.com>
2021-03-11 16:31:15 +0800

    项目初始化

---
+++ src/test/java/com/ye/elearning/ELearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ye.elearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
